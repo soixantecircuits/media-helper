7fa527b80c3b6ad5614783c26081b376ef4ff365
hugohil <hugo@soixantecircuits.fr>
2017-01-11 17:12:50 +0100

    🚀 Add AVA unit tests engine

--- tests/index.js
+++ tests/index.js
@@ -1,49 +1,35 @@
-/* globals describe it */
 'use strict'
+
+import test from 'ava'
 
 const mh = require('../media-helper')
 const path = require('path')
 const fs = require('fs')
-
-const chai = require('chai')
-const expect = chai.expect
-const chaiAsPromised = require('chai-as-promised')
-chai.use(chaiAsPromised)
 
 const base64 = fs.readFileSync(path.resolve(__dirname, 'assets/base64.txt'), { encoding: 'utf-8' })
 const filepath = path.resolve(__dirname, './assets/image.jpg')
 const URL = 'http://placehold.it/350x150'
 const buffer = Buffer.from(base64)
 
-describe('media checker', () => {
-  describe('isBase64', () => {
-    it('should return true', () => {
-      const result = mh.isBase64(base64)
-      return expect(result).to.equal(true)
+test('isBase64', (t) => {
+  const result = mh.isBase64(base64)
+  t.is(result, true)
+})
+test('isURL', (t) => {
+  const result = mh.isURL(URL)
+  t.is(result, true)
+})
+test('isFile', (t) => {
+  const result = mh.isFile(filepath)
+  t.is(result, true)
+})
+test('isBuffer', (t) => {
+  const result = mh.isBuffer(buffer)
+  t.is(result, true)
+})
+test('getMimeType on base64', (t) => {
+  mh.getMimeType(base64)
+    .then((type) => {
+      t.is(type, 'image/gif')
     })
-  })
-  describe('base64 MIME', () => {
-    it('should return image/gif', () => {
-      const result = mh.getMimeType(base64)
-      return expect(result).to.eventually.equal('image/gif')
-    })
-  })
-  describe('isURL', () => {
-    it('should return true', () => {
-      const result = mh.isURL(URL)
-      return expect(result).to.equal(true)
-    })
-  })
-  describe('isFile', () => {
-    it('should return true', () => {
-      const result = mh.isFile(filepath)
-      return expect(result).to.equal(true)
-    })
-  })
-  describe('isBuffer', () => {
-    it('should return true', () => {
-      const result = mh.isBuffer(buffer)
-      return expect(result).to.equal(true)
-    })
-  })
 })
